e2a12a7fe94c9d6dcee60d95b87f06bc2b865f98
The-Hacker894 <skylarmccauley894@gmail.com>
2019-04-28 10:00:18 -0400

    v0.8

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-cp -fv restore_cydia.sh xyz.skylarmccauley.sourceutility/sourceutility/res/
-cp -fv restore_sileo.sh xyz.skylarmccauley.sourceutility/sourceutility/res/
-cp -fv restore_zebra.sh xyz.skylarmccauley.sourceutility/sourceutility/res/
+chmod a+x xyz.skylarmccauley.sourceutility/sourceutility/res/restore_cydia.sh
+chmod a+x xyz.skylarmccauley.sourceutility/sourceutility/res/restore_sileo.sh
+chmod a+x xyz.skylarmccauley.sourceutility/sourceutility/res/restore_zebra.sh
 chmod a+x xyz.skylarmccauley.sourceutility/usr/bin/sourceutility
 chmod 0555 xyz.skylarmccauley.sourceutility/DEBIAN/postinst
+chmod 0555 xyz.skylarmccauley.sourceutility/DEBIAN/postrm
 chmod 0555 xyz.skylarmccauley.sourceutility/DEBIAN/preinst
 
 find . -name ".DS_Store" -delete
